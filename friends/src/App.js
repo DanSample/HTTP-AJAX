464ffeccd2eec0b229e5f96620e6f1a38bf97259
Dan Sample <danssample@gmail.com>
2019-06-11 22:06:26 -0700

    New File: FriendForm.js. Built out FriendForm, Converted App.js to Class Constructor, updated Index.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import Friend from './Components/Friend';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(res => {
+        this.setState(() => ({ friends: res.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.friends.map(friend => (
+          <div className="friend-container" key={friend.id}>
+            <Friend key={friend.id} friend={friend} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default App;
