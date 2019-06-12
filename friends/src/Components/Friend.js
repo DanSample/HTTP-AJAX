a3e9be3ea9e6ff8da420ec28aeead22e807a81b9
Dan Sample <danssample@gmail.com>
2019-06-11 22:38:55 -0700

    Built out Friend.js using reactstrap

--- friends/src/Components/Friend.js
+++ friends/src/Components/Friend.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Card } from 'reactstrap';
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Row,
+  Col
+} from 'reactstrap';
 
 const Friend = props => {
   const { name, age, email } = props.friend;
   return (
-    <Card>
-      <div className="name-wrapper">
-        <h1>{name}</h1>
-      </div>
-      <div className="age-wrapper">
-        <h3>{age}</h3>
-      </div>
-      <div className="email-wrapper">
-        <h3>{email}</h3>
-      </div>
-    </Card>
+    <div>
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <Card body className="text-center">
+            <CardBody>
+              <CardTitle>{name}</CardTitle>
+              <CardSubtitle>{age}</CardSubtitle>
+              <CardText>{email}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
